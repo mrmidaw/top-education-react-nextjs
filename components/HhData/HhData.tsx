0e8bf1ef18ccc2012c4f72e4bdc3fd98cebfb4b0
Vadim Gavrilov <mr.midaw@gmail.com>
2021-10-11 16:10:10 +0300

    React + Next.js 072

---
+++ components/HhData/HhData.tsx
@@ -0,0 +1,52 @@
+
+import styles from './HhData.module.css';
+import cn from 'classnames';
+import { HhDataProps } from './HhDataProps';
+import React from 'react';
+import { Card } from '..';
+import RateIcon from './rate.svg';
+
+
+
+export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDataProps): JSX.Element => {
+	return (
+		<div className={styles.hh}>
+			<Card className={styles.count}>
+				<div className={styles.Title}>Всего вакансий </div>
+				<div className={styles.countValue}>{count} </div>
+			</Card>
+
+			<Card className={styles.salary}>
+				<div>
+					<div className={styles.Title}>Начальный </div>
+					<div className={styles.salaryValue}>{juniorSalary} </div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon />
+						<RateIcon />
+					</div>
+				</div>
+
+				<div>
+					<div className={styles.Title}>Средний </div>
+					<div className={styles.salaryValue}>{middleSalary} </div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+						<RateIcon />
+					</div>
+				</div>
+
+				<div>
+					<div className={styles.Title}>Профессионал </div>
+					<div className={styles.salaryValue}>{seniorSalary} </div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+					</div>
+				</div>
+			</Card>
+		</div>
+	);
+};
